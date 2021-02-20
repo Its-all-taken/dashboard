7c86711db8ed35d3596eb781983f0a74f7703a71
Maria <kauztmaria@gmail.com>
2021-02-20 18:24:59 +0300

    hochu 4tobi vsyo zachonchilos'

---
+++ js/caru.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $('.ads-c').slick({
+    autoplay:true,
+    autoplaySpeed: 5000,
+    dots: true
+  });
+$(".close").click(function (){
+        $(".ads-over").toggleClass("blocker")
+      })
+});
+
+$(".colour").click(function (){
+  let bgColor = ["#000", "#8248CB", "#487CCB", "#CB7F48", "#507E14"];
+  $.each(bgColor, function(index, value){
+    $("body").css("background-color", value)
+  })
+});
